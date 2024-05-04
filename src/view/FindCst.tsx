df74ecdc2c4f2c08d861ab254fe398609e2c6e64
thefloppypig <67011432+thefloppypig@users.noreply.github.com>
2024-05-04 02:29:13 +0100

    a

--- src/view/FindCst.tsx
+++ src/view/FindCst.tsx
@@ -1,16 +1,30 @@
 import 'moment-timezone';
 import moment from "moment";
-import { ReactNode, useRef, useState } from "react"
+import { ReactNode, useEffect, useRef, useState } from "react"
 import './FindCst.css'
+import { Divider } from './Divider';
+import { fetchFeederList } from '../fetchUtils';
+import { FeederList } from '../shared/Types';
+import { linkWiki } from '../shared/Const';
 
 export function FindCst() {
+    const [feederList, setFeederList] = useState<FeederList>([])
+
     const fileUpload = useRef<HTMLInputElement>(null);
     const [files, setFiles] = useState<FileList | null>();
     const elems: ReactNode[] = [];
+
+    useEffect(() => {
+        fetchFeederList().then((res) => setFeederList(res));
+    }, []);
+
     if (files) {
         for (const file of files) {
             try {
-                const time = file.name.split("_")[2]?.split(".")[0];//extract time from filename
+                const nameSplit = file.name.split("_");
+                const feederId = nameSplit[1];
+                const feeder = findFeederInList(feederId, feederList);
+                const time = nameSplit[2]?.split(".")[0];//extract time from filename
                 let ms = parseInt(time);
                 let saved = "saved";
                 if (!ms || isNaN(ms)) {
@@ -25,8 +39,8 @@ export function FindCst() {
                 elems.push(<span className='fileItem' key={file.name}>
                     <h4>{file.name}</h4>
                     <div>This file was {saved} at <span className='wikiSource'>{outVisual}</span><button onClick={() => navigator.clipboard.writeText(outVisual)}>Copy</button></div>
-                    <div>Wiki Source Text for sighting:<span className='wikiSource'>{outSource}</span><button onClick={() => navigator.clipboard.writeText(outVisual)}>Copy</button></div>
-
+                    <div>Wiki Source Text for sighting:<span className='wikiSource'>{outSource}</span><button onClick={() => navigator.clipboard.writeText(outSource)}>Copy</button></div>
+                    <div>Taken in {feeder ? <a href={`${linkWiki}/${feeder.wiki}`}>{feeder.name}</a> : undefined} feeder with id {feederId}</div>
                 </span>)
             } catch (error) {
                 elems.push(<span key={file.name}>
@@ -43,6 +57,7 @@ export function FindCst() {
             <div>Simple tool to retrieve the date from the filename of a meow.camera screenshot or clip</div>
             <div>File names should look like this: <i>meow.camera_4258783365322591678_1714768691754</i></div>
             <div>Select 1 or more files:</div>
+            <Divider />
             <input type="file" ref={fileUpload} multiple onChange={(ev) => {
                 const files = ev.target.files;
                 setFiles(files);
@@ -51,7 +66,14 @@ export function FindCst() {
                     ev.currentTarget.value = "";
                     setFiles(null);
                 }} />
+            <Divider />
             {elems}
         </div>
     )
+}
+
+function findFeederInList(id: string, list: FeederList) {
+    for (const iterator of list) {
+        if (iterator.id === id) return iterator;
+    }
 }
