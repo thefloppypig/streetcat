f6b70420554837d8be4b2ff3aeb44f920aab8417
w <67011432+thefloppypig@users.noreply.github.com>
2024-06-03 16:35:00 +0100

    Cat update again

--- build/processFeederList.ts
+++ build/processFeederList.ts
@@ -1,5 +1,5 @@
 import { readFileSync, readdirSync, writeFileSync } from "node:fs";
-import { FeederData } from "../src/shared/Types"
+import { CatData, FeederData } from "../src/shared/Types"
 import { PluginOption } from "vite";
 import { feederRootPublic } from "../src/shared/Const"
 
@@ -11,10 +11,12 @@ function processFeederList() {
     for (const feeder of listDir) {
         if (feeder.isDirectory()) {
             try {
-                const index = readFileSync(`${feederRootPublic}/${feeder.name}/index.json`, "utf8");
+
+                const index = readFileSync(`${feederRootPublic}/${feeder}/index.json`, "utf8");
                 const json = JSON.parse(index) as FeederData;
                 json.__feeder = feeder.name;
                 feederIndexMap.push(json);
+                processCatList(feeder.name);
             } catch (error) {
                 console.log(`${feeder.name} has no index.json`)
             }
@@ -24,7 +26,28 @@ function processFeederList() {
     writeFileSync(`${feederRootPublic}/feederList.json`, JSON.stringify(feederIndexMap, undefined, "    "));
 }
 
+function processCatList(feeder: string) {
+    const feederPath = `${feederRootPublic}/${feeder}`;
+    const listFeeder = readdirSync(feederPath, { withFileTypes: true });
+    const catList = [] as CatData[];
 
+    for (const catDir of listFeeder) {
+        if (catDir.isDirectory()) {
+            try {
+                const catPath = `${feederRootPublic}/${catDir.name}`
+                const index = readFileSync(`${catPath}/index.json`, "utf8");
+                const json = JSON.parse(index) as CatData;
+                json.__feeder = feeder;
+                json.__cat = catDir.name;
+                catList.push(json);
+            } catch (error) {
+                console.log(`${catDir.name} has no index.json`);
+            }
+        }
+    }
+
+    writeFileSync(`${feederPath}/catList.json`, JSON.stringify(catList, undefined, "    "));
+}
 
 export const processFeederPlugin: PluginOption = {
     name: "processFeederList",
